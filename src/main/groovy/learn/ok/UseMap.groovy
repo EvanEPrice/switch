4340643680014a5b7c645eb17c9dead2ba535b92
Evan <price.evan.e@gmail.com>
2017-10-12 23:33:45 -0400

    fixing groovy map clss

--- src/main/groovy/learn/ok/UseMap.groovy
+++ src/main/groovy/learn/ok/UseMap.groovy
@@ -8,19 +8,6 @@ class UseMap implements Use {
 
     Map<Integer, String> map = [0: "zero I guess", 1:"1 or 2", 2:"1 or 2", 3:"this is getting dumb", 5:"now it's all out of order and I don't know what to do."  ]
 
-    Map<Integer, String> map = new HashMap<>()
-
-    void makeMap() {
-        map.put(0, "zero I guess")
-        map.put(1, "1 or 2")
-        map.put(2, "1 or 2")
-        map.put(3, "this is getting dumb")
-        map.put(5, "now it's all out of order and I don't know what to do.")
-    }
-
-
-
-
     @Override
     String use(int something) {
         makeMap()
